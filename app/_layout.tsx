74cb6f359d81bada3bc151bf85f8664b1df38f8c
oguzcoll <oguzcolak2000@gmail.com>
2023-12-03 22:11:36 +0300

    Modal page with reanimated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
 import CustomHeader from "@/components/CustomHeader";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import Colors from "@/constants/Colors";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "index",
 };
 
 export default function RootLayoutNav() {
+  const navigation = useNavigation();
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -14,6 +18,30 @@ export default function RootLayoutNav() {
           name="index"
           options={{
             header: () => <CustomHeader />,
+          }}
+        />
+        <Stack.Screen
+          name="(modal)/filter"
+          options={{
+            presentation: "modal",
+            headerTitle: "Filter",
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.lightGrey,
+            },
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              >
+                <Ionicons
+                  name="close-outline"
+                  size={28}
+                  color={Colors.primary}
+                />
+              </TouchableOpacity>
+            ),
           }}
         />
       </Stack>
